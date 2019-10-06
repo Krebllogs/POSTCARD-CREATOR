ee3865373a56c9332eb2b3db7a41f0b6bbe5bee5
Andrew Varriale <krebllogs@gmail.com>
2019-10-06 01:45:04 -0400

    test

---
+++ postcardcreatorWEB/app/app.js
@@ -0,0 +1,30 @@
+﻿var app = angular.module('postcardApp', ['ngRoute']);
+
+app.config(function ($routeProvider, $locationProvider) {
+    $routeProvider
+        .when("/home", {
+            controller: "postcardController",
+            templateUrl: "app/views/home.view.html"
+        })
+        .otherwise({ redirectTo: "/home" });
+
+    $locationProvider.hashPrefix('');
+});
+
+//app.config(['$httpProvider', function ($httpProvider) {
+//    //initialize get if not there
+//    if (!$httpProvider.defaults.headers.get) {
+//        $httpProvider.defaults.headers.get = {};
+//    }
+
+//    // Answer edited to include suggestions from comments
+//    // because previous version of code introduced browser-related errors
+
+//    //disable IE ajax request caching
+//    $httpProvider.defaults.headers.get['If-Modified-Since'] = 'Mon, 26 Jul 1997 05:00:00 GMT';
+
+//    // extra
+//    $httpProvider.defaults.headers.get['Cache-Control'] = 'no-cache';
+//    $httpProvider.defaults.headers.get['Pragma'] = 'no-cache';
+//    $httpProvider.interceptors.push('authenticationInterceptorService');
+//}]);
---
+++ postcardcreatorWEB/app/controllers/postcardController.js
@@ -0,0 +1,18 @@
+﻿//app.controller('postcardController', function ($scope)
+//{
+//    $scope.Message = "Hello To Postcard Creator WEB";
+//}
+//);
+
+(function () {
+    'use strict';
+    app.controller('postcardController', [
+        '$scope', '$location', function ($scope, $location) {
+
+            $scope.Message = "Hello To Postcard Creator WEB";
+
+            $scope.go = function () {
+                $location.path('/begin');
+            };
+        }]);
+}).call(this);
